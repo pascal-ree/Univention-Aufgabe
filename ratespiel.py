5833e728ed815f2888f6e3ec6f553545b991d28b
pascal-ree <98171382+pascal-ree@users.noreply.github.com>
2022-01-21 18:19:48 +0100

    Update ratespiel.py

--- ratespiel.py
+++ ratespiel.py
@@ -4,35 +4,35 @@ import random
 
 
 def user_input():
-    # prüft Eingabe - nur reelle Zahlen von 0 - 100 sind Erlaubt
+    # prüft Eingabe - nur reelle Zahlen von 0 - 100 sind erlaubt
     while True:
         try:
             user_choice = int(input("Dein Tipp: "))
             if user_choice < 0 or user_choice > 100:
                 raise ValueError
         except ValueError:
-            print("Achtung es sind nur reelle Zahlen im Bereuch 0-100 erlaubt!")
+            print("Achtung es sind nur reelle Zahlen im Bereich 0-100 erlaubt!")
         else:
             return user_choice
 
 
 def guess_number():
     attempts = 0
-    # generiert die zufällige Zahl unter benutzung der random Bibliothek
+    # generiert die zufaellige Zahl unter Benutzung der random Bibliothek
     wanted_number = random.randint(0, 100)
 
-    print("Das Spiel hat eine zufallige Zahl zwischen 0 und 100 generiert.")
+    print("Das Spiel hat eine zufaellige Zahl zwischen 0 und 100 generiert.")
     while True:
-        # Zählt die gebrauchten Versuche (Fehlerhafte Eingaben zählen nicht als Versuch)
+        # Zählt die gebrauchten Versuche (Fehlerhafte Eingaben zaehlen nicht als Versuch)
         attempts += 1
         user_choice = user_input()
         if user_choice != wanted_number:
-            # Gibt eine Lösungshilfe wieder
+            # Gibt eine Loesungshilfe wieder
             user_help = "kleiner" if user_choice > wanted_number else "groesser"
             print(f"Die gesuchte Zahl ist {user_help} als {user_choice}. Versuche es nochmal!")
             continue
         # Ist die richtige Zahl gefunden worden, wird sie zusammen mit den gebrauchten Versuchen ausgegeben
-        print(f"Richtig! Die gesuchte Zahl war {wanted_number}. Du hast {attempts} Versuche gebraucht!")
+        print(f"Richtig! Die gesuchte Zahl ist {wanted_number}. Du hast {attempts} Versuche gebraucht!")
         break
 
 
